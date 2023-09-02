0280376aec93b85e56921d7421988c79f9c85941
Enzo Santos <enzo.santos29@hotmail.com>
2023-09-02 13:54:50 -0300

    Melhora seleção de jogos por botão

--- src/main/kotlin/screens/Main.kt
+++ src/main/kotlin/screens/Main.kt
@@ -7,27 +7,25 @@ import csstype.FlexDirection
 import emotion.react.css
 import react.FC
 import react.Props
-import react.create
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.p
 import react.dom.svg.ReactSVG.path
 import react.dom.svg.ReactSVG.svg
 import react.useState
 
-external interface MainProps : Props
-
 enum class GameType { SUDOKU, WORDLE, TZFE }
 
 data class MainState(
+    val availableGameTypes: Set<GameType>,
     val currentGameType: GameType,
     val buttonPressed: Boolean?
 )
 
-external interface IconProps : Props {
+external interface PixelatedButtonProps : Props {
     var enabled: Boolean?
 }
 
-val Icon = FC<IconProps> { props ->
+val PixelatedButton = FC<PixelatedButtonProps> { props ->
     val enabled = props.enabled
     val o = if (enabled == null) 0 else if (enabled) 4 else 2
     val k = 5.0
@@ -36,49 +34,143 @@ val Icon = FC<IconProps> { props ->
         width = 25 * k
         height = 18 * k
         xmlns = "http://www.w3.org/2000/svg"
-        viewBox = "0 -0.5 25 18"
+        viewBox = listOf("0", "-0.5", "25", "18").joinToString(" ")
         shapeRendering = "crispEdges"
         path {
             val b = 0
             stroke = "#e63c45"
-            d =
-                "M9 ${b + 0 + o}h7M6 ${b + 1 + o}h13M4 ${b + 2 + o}h17M3 ${b + 3 + o}h19M2 ${b + 4 + o}h21M2 ${b + 5 + o}h21M3 ${b + 6 + o}h19M4 ${b + 7 + o}h17M6 ${b + 8 + o}h13M9 ${b + 9 + o}h7"
+            d = listOf(
+                "M9",
+                "${b + 0 + o}h7M6",
+                "${b + 1 + o}h13M4",
+                "${b + 2 + o}h17M3",
+                "${b + 3 + o}h19M2",
+                "${b + 4 + o}h21M2",
+                "${b + 5 + o}h21M3",
+                "${b + 6 + o}h19M4",
+                "${b + 7 + o}h17M6",
+                "${b + 8 + o}h13M9",
+                "${b + 9 + o}h7",
+            ).joinToString(" ")
         }
         path {
             stroke = "#5a617c"
-            d = "M1 8h1M0 9h2M0 10h2M0 11h3M1 12h3M2 13h4M3 14h6M5 15h7M8 16h2"
+            d = listOf(
+                "M1",
+                "8h1M0",
+                "9h2M0",
+                "10h2M0",
+                "11h3M1",
+                "12h3M2",
+                "13h4M3",
+                "14h6M5",
+                "15h7M8",
+                "16h2",
+            ).joinToString(" ")
         }
         path {
             val b = 6
             stroke = "#f67732"
-            d = "M2 ${b + o}h1M3 ${b + 1 + o}h1M4 ${b + 2 + o}h2M6 ${b + 3 + o}h3M9 ${b + 4 + o}h4"
+            d = listOf(
+                "M2",
+                "${b + 0 + o}h1M3",
+                "${b + 1 + o}h1M4",
+                "${b + 2 + o}h2M6",
+                "${b + 3 + o}h3M9",
+                "${b + 4 + o}h4",
+            ).joinToString(" ")
         }
         path {
             val b = 6
             stroke = "#fea44a"
-            d = "M22 ${b + o}h1M21 ${b + 1 + o}h1M19 ${b + 2 + o}h2M16 ${b + 3 + o}h3M13 ${b + 4 + o}h3"
+            d = listOf(
+                "M22",
+                "${b + 0 + o}h1M21",
+                "${b + 1 + o}h1M19",
+                "${b + 2 + o}h2M16",
+                "${b + 3 + o}h3M13",
+                "${b + 4 + o}h3",
+            ).joinToString(" ")
         }
         if (enabled == null) {
             path {
                 stroke = "#ab1719"
-                d = "M2 7h1M22 7h1M2 8h2M21 8h2M3 9h3M19 9h3M4 10h5M16 10h5M6 11h13M9 12h7"
+                d = listOf(
+                    "M2",
+                    "7h1M22",
+                    "7h1M2",
+                    "8h2M21",
+                    "8h2M3",
+                    "9h3M19",
+                    "9h3M4",
+                    "10h5M16",
+                    "10h5M6",
+                    "11h13M9",
+                    "12h7",
+                ).joinToString(" ")
             }
         }
         path {
             stroke = "#90a1b9"
-            d = "M23 8h1M23 9h2M23 10h2M22 11h3M21 12h3M19 13h4M16 14h6M15 15h5M13 16h4"
+            d = listOf(
+                "M23",
+                "8h1M23",
+                "9h2M23",
+                "10h2M22",
+                "11h3M21",
+                "12h3M19",
+                "13h4M16",
+                "14h6M15",
+                "15h5M13",
+                "16h4",
+            ).joinToString(" ")
         }
         path {
             stroke = "#9a0e19"
-            d = "M2 9h1M22 9h1M3 10h1M21 10h1M4 11h2M19 11h2M6 12h3M16 12h3M9 13h7"
+            d = listOf(
+                "M2",
+                "9h1M22",
+                "9h1M3",
+                "10h1M21",
+                "10h1M4",
+                "11h2M19",
+                "11h2M6",
+                "12h3M16",
+                "12h3M9",
+                "13h7",
+            ).joinToString(" ")
         }
         path {
             stroke = "#860117"
-            d = "M2 10h1M22 10h1M3 11h1M21 11h1M4 12h2M19 12h2M6 13h3M16 13h3M9 14h7"
+            d = listOf(
+                "M2",
+                "10h1M22",
+                "10h1M3",
+                "11h1M21",
+                "11h1M4",
+                "12h2M19",
+                "12h2M6",
+                "13h3M16",
+                "13h3M9",
+                "14h7",
+            ).joinToString(" ")
         }
         path {
             stroke = "#3d4e68"
-            d = "M0 12h1M24 12h1M1 13h1M23 13h1M2 14h1M22 14h1M3 15h2M20 15h2M5 16h3M17 16h3M8 17h9"
+            d = listOf(
+                "M0",
+                "12h1M24",
+                "12h1M1",
+                "13h1M23",
+                "13h1M2",
+                "14h1M22",
+                "14h1M3",
+                "15h2M20",
+                "15h2M5",
+                "16h3M17",
+                "16h3M8",
+                "17h9",
+            ).joinToString(" ")
         }
         path {
             stroke = "#6c819a"
@@ -87,13 +179,18 @@ val Icon = FC<IconProps> { props ->
     }
 }
 
-val Main = FC<MainProps> { _ ->
-    var state by useState(
+val Main = FC<Props> { _ ->
+    var state by useState {
+        val gameTypes = GameType.values().toMutableSet()
+        val gameType = gameTypes.random()
+        gameTypes.remove(gameType)
+
         MainState(
-            currentGameType = GameType.values().random(),
+            availableGameTypes = gameTypes,
+            currentGameType = gameType,
             buttonPressed = null,
         )
-    )
+    }
     div {
         css {
             display = Display.flex
@@ -115,13 +212,19 @@ val Main = FC<MainProps> { _ ->
                 state = state.copy(buttonPressed = false)
             }
             onClick = { _ ->
-                var nextGameType: GameType
-                do {
-                    nextGameType = GameType.values().random()
-                } while (nextGameType == state.currentGameType)
-                state = state.copy(currentGameType = nextGameType)
+                val nextAvailableGameTypes = state.availableGameTypes
+                    .ifEmpty { GameType.values().asIterable() }
+                    .toMutableSet()
+
+                val nextGameType = nextAvailableGameTypes.random()
+                nextAvailableGameTypes.remove(nextGameType)
+
+                state = state.copy(
+                    availableGameTypes = nextAvailableGameTypes,
+                    currentGameType = nextGameType,
+                )
             }
-            +Icon.create {
+            PixelatedButton {
                 enabled = state.buttonPressed
             }
         }
@@ -132,10 +235,10 @@ val Main = FC<MainProps> { _ ->
                 GameType.TZFE -> "2048"
             }
         }
-        +when (state.currentGameType) {
-            GameType.SUDOKU -> Sudoku.create {}
-            GameType.WORDLE -> Wordle.create {}
-            GameType.TZFE -> TZFE.create {}
+        when (state.currentGameType) {
+            GameType.SUDOKU -> Sudoku {}
+            GameType.WORDLE -> Wordle {}
+            GameType.TZFE -> TZFE {}
         }
     }
 }
